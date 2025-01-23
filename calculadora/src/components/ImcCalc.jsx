5a854249eb877a24c256eaef3c78861a6a21b19b
Yago Cima Castelao <yago_castelau@hotmail.com>
2025-01-23 03:02:28 +0100

    Creado Componente de Calculadora

---
+++ calculadora/src/components/ImcCalc.jsx
@@ -0,0 +1,37 @@
+import styles from './ImcCalc.module.css'
+
+const ImcCalc = () => {
+  return (
+    <div id="calc-container">
+      <h2>Calculadora de IMC</h2>
+      <form id="imc-form">
+        <div className="form-inputs">
+          <div className="form-control">
+            <label htmlFor="height">Altura:</label>
+            <input
+              type="text"
+              name="height"
+              id="height"
+              placeholder="Ejemplo 1.75"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="weight">Peso:</label>
+            <input
+              type="text"
+              name="weight"
+              id="weight"
+              placeholder="Ejemplo 70.5"
+            />
+          </div>
+        </div>
+        <div className="action-control">
+            <button>Calcular</button>
+            <button>Limpiar</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ImcCalc
