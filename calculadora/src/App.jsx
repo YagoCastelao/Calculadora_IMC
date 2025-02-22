277325545bf67e13fbf2db0922b1dcd1b3f0ed5b
Yago Cima Castelao <yago_castelau@hotmail.com>
2025-02-22 01:56:57 +0100

    Iniciando componente da tabela de informações

--- calculadora/src/App.jsx
+++ calculadora/src/App.jsx
@@ -1,10 +1,25 @@
-import "./App.css";
+import { useState } from "react";
+import { data } from "./data/data";
+
 import ImcCalc from "./components/ImcCalc";
+import ImcTable from "./components/ImcTable";
+
+import "./App.css";
 
 function App() {
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
+  const calcImc = (e, height, weight) => {
+    e.preventDefault();
+
+    console.log('executou')
+  };
+
   return (
     <div className="container">
-      <ImcCalc />
+      {!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable />}
     </div>
   );
 }
